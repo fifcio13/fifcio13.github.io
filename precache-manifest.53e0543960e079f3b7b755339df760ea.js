9455df1bcd1babbbef121dc33917257a4b54989e
filipMint <filipson20@gmail.com>
2020-09-17 16:56:16 +0200

    Updates

---
+++ precache-manifest.53e0543960e079f3b7b755339df760ea.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d7cda1f43cc2922a2f13cbaf6b62e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9564c22fd7b68ce0b0a1",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "8ba6407648ced5abd12a",
+    "url": "/static/css/main.eed48c7c.chunk.css"
+  },
+  {
+    "revision": "9564c22fd7b68ce0b0a1",
+    "url": "/static/js/2.308fd3cb.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.308fd3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba6407648ced5abd12a",
+    "url": "/static/js/main.e87b8ec6.chunk.js"
+  },
+  {
+    "revision": "dcc6da2a38d32b4955bf",
+    "url": "/static/js/runtime-main.3b0c188f.js"
+  },
+  {
+    "revision": "761d47d49e951a23d981dc93411c6b33",
+    "url": "/static/media/azon-thumb.761d47d4.png"
+  },
+  {
+    "revision": "d8c3a0013515cd1628454c94c0b7363a",
+    "url": "/static/media/azon-thumb.d8c3a001.webp"
+  },
+  {
+    "revision": "ff6d266dd5394496ed234c8440944b3b",
+    "url": "/static/media/azon.ff6d266d.png"
+  },
+  {
+    "revision": "1837d35dcaebfe75f1d644da3d348b3a",
+    "url": "/static/media/cart.1837d35d.svg"
+  },
+  {
+    "revision": "ba36d463c417406bc2ca5fcaea67ce00",
+    "url": "/static/media/email.ba36d463.svg"
+  },
+  {
+    "revision": "89e66af7e447ce82fa648da6247d0ddc",
+    "url": "/static/media/gear.89e66af7.svg"
+  },
+  {
+    "revision": "4659a5c74855a8af214a4a5dee3d617e",
+    "url": "/static/media/github.4659a5c7.svg"
+  },
+  {
+    "revision": "183ec2b2f0ca6b15c048bafa0a90f78a",
+    "url": "/static/media/kuban-thumb.183ec2b2.png"
+  },
+  {
+    "revision": "b9a467c1e6faf2f02e8e6aeaf3fbf687",
+    "url": "/static/media/kuban-thumb.b9a467c1.webp"
+  },
+  {
+    "revision": "5c69d8962c6270e7b95bb3aa3e661b3c",
+    "url": "/static/media/kuban.5c69d896.png"
+  },
+  {
+    "revision": "b6ec9c92792977009c84bb5b7be2c998",
+    "url": "/static/media/mobile.b6ec9c92.svg"
+  },
+  {
+    "revision": "08f61385e314b910e3cb94faf762ca37",
+    "url": "/static/media/pro.08f61385.webp"
+  },
+  {
+    "revision": "5f73bc0a0b2f275608790711f8924cf9",
+    "url": "/static/media/pro.5f73bc0a.png"
+  },
+  {
+    "revision": "806ecc2eef879b6543d6a5fdcf8338db",
+    "url": "/static/media/web.806ecc2e.svg"
+  }
+]);
